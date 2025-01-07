24c9fd920fda1eacad37fb7f6a6750eaddeab1db
firstPush <adel.sdet2@gmail.com>
2025-01-07 11:39:52 -0600

    another solution

--- src/main/java/org/example/LengthofLastWord.java
+++ src/main/java/org/example/LengthofLastWord.java
@@ -1,12 +1,15 @@
-package org.example;
 
-public class LengthofLastWord {
-    public int lengthOfLastWord (String s) {
-        String trimmed = s.trim();
-        int lastIndexOf = trimmed.lastIndexOf(' ');
-        String lastWord = trimmed.substring(lastIndexOf + 1);
-        int length = lastWord.length();
-        return length;
-    }
 
-}
+//public int lengthOfLastWord(String s) {
+//    int end = s.length() - 1;
+//
+//    while (end >= 0 && s.charAt(end) == ' ') {
+//        end--;
+//    }
+//
+//    int start = end;
+//    while (start >= 0 && s.charAt(start) != ' ') {
+//        start--;
+//    }
+//
+//    return end - start;
