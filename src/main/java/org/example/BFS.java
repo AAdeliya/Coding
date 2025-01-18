c788867ed39ca4b90b338b27d4da34eb69720132
firstPush <adel.sdet2@gmail.com>
2025-01-18 00:20:43 -0600

    Breadth-First Search (BFS)

---
+++ src/main/java/org/example/BFS.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class BFS {
+//    BFS(graph, start):
+//    Create a queue (Q) and enqueue the start node
+//    Mark the start node as visited
+//
+//    while Q is not empty:
+//    current = dequeue(Q)
+//    Process the current node
+//
+//        for each neighbor of current:
+//            if neighbor is not visited:
+//    Mark neighbor as visited
+//    Enqueue neighbor into Q
+}
