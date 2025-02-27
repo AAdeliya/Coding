90d3d38163b1075b7ee40675ce862d948da490c4
firstPush <adel.sdet2@gmail.com>
2025-02-27 13:06:11 -0600

    Is Subsequence

---
+++ src/main/java/org/example/IsSubsequence.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class IsSubsequence {
+    public boolean isSubsequence(String s, String t) {
+        int sp = 0;
+        int tp = 0;
+
+        while ( sp < s.length() && tp < t.length()) {
+            if (s.charAt(sp) == t.charAt(tp)) {
+                sp++;
+            }
+
+            tp++;
+        }
+
+        return  sp == s.length();
+    }
+
+}
