1dbab7e7848e1504804629e43ba855f2056e2dc0
firstPush <adel.sdet2@gmail.com>
2025-03-08 20:43:47 -0600

    First Missing positive

---
+++ src/main/java/org/example/FirstMissingPositive.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class FirstMissingPositive {
+    public int firstMissingPositive(int[] nums) {
+        Arrays.sort(nums);
+        int target = 1;
+
+        for (int n: nums) {
+            if (n > 0 && n == target) {
+                target++;
+            } else if (n > target) {
+                return target;
+            }
+        }
+
+        return target;
+
+    }
+}
