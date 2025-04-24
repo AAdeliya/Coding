71ffe5dabee10b61687770f6146de6726e092f72
firstPush <adel.sdet2@gmail.com>
2025-04-24 09:26:37 -0500

    Path Sum

---
+++ src/main/java/org/example/Paths.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import javax.swing.tree.TreeNode;
+
+public class Paths {
+      public class TreeNode {
+         int val;
+ TreeNode left;
+ TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+         this.left = left;
+         this.right = right;
+ }
+ }
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.left == null && root.right == null) {
+            return targetSum = root.val;
+        }
+
+        boolean leftSum = hasPathSum(root.left, targetSum - root.val);
+        boolean rightSum = hasPathSum(root.right, targetSum - root.val);
+
+        return leftSum || rightSum;
+    }
+
+}
