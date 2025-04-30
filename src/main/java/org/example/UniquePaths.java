76285f9b3bd5581f813324d16bb34e4108d48ea7
firstPush <adel.sdet2@gmail.com>
2025-04-30 17:15:54 -0500

    unique path (recursion, dfs, combinatorial approach (binomial coefficient)

--- src/main/java/org/example/UniquePaths.java
+++ src/main/java/org/example/UniquePaths.java
@@ -17,3 +17,39 @@ public class UniquePaths {
         return memo[i][j];
     }
 }
+
+//Dynamic programming
+public int uniquePathDP(int m, int n) {
+    int[][] dp = new int[m][n];
+
+    for (int i = 0; i < m; i++) dp[i][0] = 1;
+    for (int j = 0; j  < n; j++) dp[0][j] = 1;
+
+    for (int i = 1; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        }
+    }
+
+    return dp[m - 1][n - 1];
+}
+
+//Combinatorial approach - time O(min(m,n))
+                           //space O(1) - no arrays, no recursion
+
+public int uniquePathCombinatorialApproach(int m , int n) {
+    return (int) comb(m + n - 2, Math.min(m - 1, n - 1));
+
+}
+
+private long comb(int a, int b) {
+    long res = 1;
+    for (int i = 1; i <= b; i++) {
+        res = res * (a - i + 1) / i;
+
+    }
+
+    return res;
+
+}
+
